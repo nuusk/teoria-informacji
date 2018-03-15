a5b7f003b0e1052b5d46475b145ab1e5d27e54ca
Piotr Ptak <robakzdobywca@gmail.com>
2018-03-15 09:26:21 +0100

    test chart

--- lab1/testChart.js
+++ lab1/testChart.js
@@ -2,15 +2,15 @@ const path = require('path');
 require('dotenv').config({path: path.join(__dirname, "../.env")});
 const plotly = require('plotly')(process.env.PLOTLY_USERNAME, process.env.PLOTLY_API_KEY);
 
-var data = [
+let data = [
   {
     x: ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"],
     y: [1, 3, 6],
     type: "scatter"
   }
 ];
-var graphOptions = {filename: "date-axes", fileopt: "overwrite"};
+const graphOptions = {filename: "date-axes", fileopt: "overwrite"};
 
-plotly.plot(data, graphOptions, function (err, msg) {
+plotly.plot(data, graphOptions, (err, msg) => {
     console.log(msg);
 });
