724ac8d5b565092213e68d2f743ea87abc6c8767
Piotr Ptak <robakzdobywca@gmail.com>
2018-04-11 20:09:13 +0200

    tylko raz liczymy counter, kurde.

--- entropy/entropy-letters.js
+++ entropy/entropy-letters.js
@@ -11,19 +11,32 @@ Korzystając z zaobserwowanych wartości entropii warunkowej odpowiedź na py
 */
 
 
-// aktualny rzad wyliczanej entropii
+// poczatkowy rzad wyliczanej entropii (bedzie sie zwiekszal az do maksymalnej wartosci)
 // rzad wiekszy od 0 to entropia warunkowa - czyli rozpatrujemy odpowiednia ilosc poprzednich stanow (liter badz slow)
-let ORDER = 3;
+let ORDER = 0;
+const MAX_ORDER = 5;
+const LANGUAGE = 'en'; // opcje do wyboru : [en, eo, et, ht, la, nv, so]
 
 const fs = require('fs');
-let input = fs.readFileSync('./data/norm_wiki_en.txt', 'utf8').split('\n')[0];
+let input = fs.readFileSync(`./data/norm_wiki_${LANGUAGE}.txt`, 'utf8').split('\n')[0];
 let english = "abcdefghijklmnopqrstuvwxyz1234567890 ".split("");
 
 // dane do lancucha markova. tabelka zawierajaca odpowiednie stany i mozliwosci przejscia do kolejnego stanu
 let chain = {};
 
+// obiekt zawierajacy liczbe wystapien kazdej litery. potrzebne do wyliczenia prawdopobienstwa wystapienia liter niezaleznie od aktualnego stanu
+let counter = {
+  'a': 0,'b': 0,'c': 0,'d': 0,'e': 0,'f': 0,'g': 0,'h': 0,
+  'i': 0,'j': 0,'k': 0,'l': 0,'m': 0,'n': 0,'o': 0,'p': 0,
+  'q': 0,'r': 0,'s': 0,'t': 0,'u': 0,'v': 0,'w': 0,'x': 0,
+  'y': 0,'z': 0,'1': 0,'2': 0,'3': 0,'4': 0,'5': 0,'6': 0,
+  '7': 0,'8': 0,'9': 0,'0': 0, ' ': 0
+};
+// zliczamy je tylko raz. tj. przy pierwszej iteracji petli (dla rzedu == ORDER.)
+let alreadyCountered = false;
+
 let letters = input.split('');
-while (ORDER <= 3) {
+while (ORDER <= MAX_ORDER) {
   for (let i=0; i<letters.length-1-ORDER; i++) {
     let currentGram = '';
     for (let j=0; j<ORDER; j++) {
@@ -49,8 +62,71 @@ while (ORDER <= 3) {
         chain[currentGram]["!counter"]++;
       }
     }
+    
+    // inkrementujemy rowniez liczbe nastepnej litery. potrzebne do wyliczenia prawdopobobienstwa zadanej litery
+    if (!alreadyCountered)
+      counter[nextLetter]++;
   };
+  alreadyCountered = true;
   
+  // liczenie entropii. zgodnie ze wzorem (jesli ORDER>0, to wzor na entropie warunkowa)
+  let currentSum = 0
+  // lacznie rozpatrujemy letters.length -1 -ORDER stanow. bedzie to przydatne do obliczenia prawdopobobienstwa zajscia zdarzenia losowego X (tj. konkretnego n-gramu)
+  let numberOfGrams = letters.length - 1 - ORDER;
+  for (state in chain) {
+    // w tym momencie rozpatrujemy tylko stany o dlugosci rownej aktualnemu ORDEROWI. poprzednie stany juz rozpatrzelismy w poprzedniej iteracji
+    if (state.length != ORDER) continue;
+
+    // prawdopobobiestwo danego stanu to iloraz tego ile razy ten stan wystapil przez liczbe wszystkich stanow
+    let stateProbability = chain[state]["!counter"] / numberOfGrams;
+    for (nextLetter in chain[state]) {
+      // prawdopobobienstwo zajscia nastpenej litery niezaleznie od aktualnego stanu
+      let nextLetterProbability = counter[nextLetter] / numberOfGrams;
+      if (!nextLetterProbability) {
+        console.log(`cos nie gra z ${nextLetter} w stanie ${state}`);
+      }
+      // console.log(`nextLetterProbability: ${nextLetterProbability}`);
+
+      // prawdopobobienstwo laczne X i Y to iloczyn prawd. zajscia zdarzenia X i prawd. zajscia zdarzenia Y
+      let combinedProbability = stateProbability * nextLetterProbability;
+      // console.log(`combinedProbability: ${combinedProbability}`);
+
+      // prawdopobobienstwo warunkowe wyliczamy na podstawie wyznaczonych wczesniej w obiekcie chain wartosci
+      let conditionalProbability = chain[state][nextLetter] / chain[state]["!counter"];
+      // console.log(`conditionalProbability: ${conditionalProbability}`);
+  
+      // ze wzoru, dodajemy do naliczanej sumy iloczyn prawd.lacznego i logarytmu(2) z prawd. warunkowego
+      currentSum += combinedProbability * Math.log2(conditionalProbability);
+      // console.log(`currentSum: ${currentSum}`);
+      
+      if (conditionalProbability > 1 || combinedProbability >1 || stateProbability > 1 || nextLetterProbability > 1) {
+        console.log(`prof. weglarz nie bylby z ciebie dumny. popatrz co narobiles:`);
+        // console.log(`conditionalProbability: ${conditionalProbability}\n combinedProbability: ${combinedProbability}\n stateProbability: ${stateProbability}\n nextLetterProbability: ${nextLetterProbability}\n`);
+        console.log(`state: ${state}`);
+        console.log(`nextLetter: ${nextLetter}`);
+        console.log(`chain:`);
+        console.log(chain[state]);
+        console.log('~~~~~~~~~~');
+      }
+    }
+  }
+
+  // ostatecznie entropia to obliczona przez nas suma pomzozona razy -1
+  let entropy = currentSum * (-1);
+
+  console.log(`Entropia warunkwa rzedu ${ORDER} dla jezyka ${LANGUAGE} wynosi ${entropy}.`);
+
+// probability.forEach(letterProbability => {
+//   // console.log(letterProbability);letterProbability
+//   currentSum += Math.log2(letterProbability)*letterProbability;
+// });
+
+// entropy = -currentSum;
+
+// console.log(`The entropy of this language is ${entropy}.`);
+
+
+
   ORDER++;
 }
 
