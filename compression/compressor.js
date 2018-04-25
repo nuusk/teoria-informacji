29ee55ec959e3a60ca27756179ebb948a908a0a6
Piotr Ptak <robakzdobywca@gmail.com>
2018-04-25 08:53:32 +0200

    compression init.

---
+++ compression/compressor.js
@@ -0,0 +1,31 @@
+class Compressor {
+
+  create(text) {
+    let code;
+    // tworzymy kod na podstawie którego będziemy kompresować dane
+
+    return code;
+  }
+
+  encode(uncompressed) {
+    let compressed;
+    // kompresujemy tekst na podstawie kodu
+
+    return compressed;
+  }
+
+  decode(compressed) {
+    let uncompressed;
+
+    return uncompressed;
+  }
+
+  save() {
+
+  }
+
+  load() {
+
+  }
+
+}
