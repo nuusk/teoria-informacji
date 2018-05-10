c12b3456ae42da61aece9e60d28adcce069360bc
Piotr Ptak <robakzdobywca@gmail.com>
2018-05-10 14:27:29 +0200

    compression, first stage

--- compression/app.js
+++ compression/app.js
@@ -1,6 +1,17 @@
-const Compressor = require('./modules/compressor');
+const Compressor = require('./modules/Compressor');
 const compressor = new Compressor();
 
-let alphabet = ['a', 'b', 'c'];
+const testFile = 'data/test.txt';
+// const wikiFile = 'data/norm_wiki_sample.txt';
 
-compressor.create(alphabet);
+const ENCODE_NAME = 'ENCODED';
+const CODE_NAME = 'CODE';
+const DECODED_NAME = 'DECODED';
+
+compressor.create(testFile);
+// console.log(compressor._code);
+compressor.encode(testFile);
+compressor.save(ENCODE_NAME, CODE_NAME);
+
+compressor.load(ENCODE_NAME, CODE_NAME);
+compressor.decode();
--- compression/modules/Compressor.js
+++ compression/modules/Compressor.js
@@ -1,36 +1,212 @@
+const fs = require('fs');
+const BitArray  = require('./BitArray');
+
 class Compressor {
-
-  create(alphabet) {
-    console.log(alphabet);
-
-    let code;
-    // tworzymy kod na podstawie którego będziemy kompresować dane
-
-    return code;
+  constructor() {
+    //obiekt zawierajacy kod
+    this.CODE = {};
+    this.BYTES = null;
   }
 
-  encode(uncompressed) {
-    let compressed;
-    // kompresujemy tekst na podstawie kodu
+  // do wczytywania this.CODE
+  GET_CODE(fileName) {
+    const input = fs.readFileSync(fileName, 'utf8').split('\n');
 
-    return compressed;
+    for (let line of input) {
+      if (line) {
+        let [key, value] = line.split(':');
+        if (key === 'size') {
+          this.CODE = Object.defineProperty({}, 'size', {
+            value: parseInt(value),
+            enumerable: false
+          });
+        } else {
+          this.CODE[key] = new BitArray(this.CODE.size, value);
+        }
+      }
+    }
   }
 
-  decode(compressed) {
-    let uncompressed;
+  // do wczytywania this.BYTES
+  GET_BYTES(fileName) {
+    // okreslamy jak duzymi chunkami bedziemy pobierac dane
+    const CHUNK_SIZE = 1048576;   //1 Mb
+    
+    const fd = fs.openSync(fileName, 'r+');
+    let bytesRead = 0;
+    let currentByte = 0;
+    let bytes = [];
 
-    return uncompressed;
+    do {
+      try {
+        const buffer = new Uint8Array(CHUNK_SIZE);
+        // odczytujemy synchronicznie dane z buforu 
+        bytesRead = fs.readSync(fd, buffer, 0, CHUNK_SIZE);
+        
+        // odczytywany bajt nalezy powiekszyc o wartosc bytow ktore odczytalismy
+        currentByte += bytesRead;
+
+        // DO BAJTOW ktore byly wczesniej dorzcam to co w buforze
+        bytes = [...bytes, ...buffer.slice(0, bytesRead)];
+      } catch (e) {
+        console.error(e);
+        break;
+      }
+    } while (bytesRead === CHUNK_SIZE);
+
+    this.BYTES = bytes;
   }
 
-  save() {
-    // zapisz kod oraz zakodowany tekst
+  create(inputFilePath) {
+    const inputText = fs.readFileSync(inputFilePath, 'utf8').split('\n')[0];
+    const letters = inputText.split('');
+    const density = {};
 
+    // na poczatku tworze liste czestosci wystapien znakow
+    letters.forEach(letter => {
+      if (density[letter]) {
+        density[letter]++;
+      } else {
+        density[letter]=1;
+      }
+    });
+
+    // definiuje tablice zawierającą nazwy propertiesów obiektu density
+    const keys = Object.keys(density);
+
+    // wyznaczamy jaką wielkość moze osiagac kod ktory tworzymy
+    this.CODE = Object.defineProperty({}, 'size', {
+      value: Math.ceil(Math.log2(keys.length)),
+      enumerable: false
+    });
+
+    // sortujemy w tym celu, by najczesciej uzywane znaki otrzymaly jak najmniejsza wartosc
+    const sortedCounter = keys
+      .map(key => ({
+        char: key,
+        density: density[key]
+      }))
+      .sort((a, b) => {
+        if (a.density < b.density) return 1;
+        else if (a.density === b.density) return 0;
+        else return -1;
+      });
+
+    // dla kazdego klucza w kodzie tworzymy nowa wartosc (ciag 0 i 1)
+    sortedCounter.forEach((element, index) => {
+      this.CODE[element.char] = new BitArray(this.CODE.size, index);
+    });
   }
 
-  load() {
-    // wczytaj zakodowany tekst oraz kod
+  encode(filePath) {
+    const inputLetters = fs.readFileSync(filePath, 'utf8').split('\n')[0].split('');
+    
+    const data = inputLetters
+      .map(letter => this.CODE[letter].toString())
+      .join('');
 
+    console.log(`encoding data: ${data}`);
+
+    const numBytes = Math.ceil(data.length / 8) + 1;
+    this.BYTES = new Uint8Array(numBytes);
+    // console.log(`this.BYTES: ${this.BYTES}`);
+
+    const byteOffset = data.length % 8;
+
+    this.BYTES[0] = byteOffset;
+
+    let i = 0, j = 1;
+    while(i < data.length && j < numBytes) {
+      const dataChunk = data.slice(i, i + 8);
+      this.BYTES[j] = new BitArray(8, dataChunk).toNumber();
+      i += 8;
+      j++;
+    }
+    console.log(`after while - this.BYTES: ${this.BYTES}`);
+
+    // jesli cos zostalo do wpisania
+    if (byteOffset) {
+      const dataChunk = data.slice(i, i + byteOffset);
+      // zamiast 8 bitowej tablicy robimy byteOffset
+      this.BYTES[j] = new BitArray(byteOffset, dataChunk).toNumber();
+      // console.log(`byteOffset - this.BYTES: ${this.BYTES}`);
+    }
   }
+
+  decode() {
+    // sprawdzamy dlugosc offsetu (ogona)
+    const byteOffset = this.BYTES[0];
+    let data = '';
+    let decoded = '';
+
+    // jesli ogon = 0 to biore wszystkie bity, inaczej tylko do tego ostatniego
+    let endPoint;
+    if (byteOffset == 0) {
+      endPoint = this.BYTES.length;
+    } else {
+      endPoint = this.BYTES.length - 1;
+    }
+
+    for (let i = 1; i < endPoint; i++) {
+      // data to dlugi string 0 i 1
+      data += new BitArray(8, this.BYTES[i]).toString();
+    }
+
+    if (byteOffset) {
+      // jesli ogon byl niezerowy to dorzucam do niego bity o dlugosci tego lancucha
+      data += new BitArray(byteOffset, this.BYTES[this.BYTES.length - 1]).toString();
+    }
+
+    // console.log(`Data while decoding: ${data}`);
+
+    // idziemy po calym ciagu data
+    // czytamy dane w zaleznosci od wartosci size, ktora zdefiniowalismy na poczatku
+    for (let i = 0; i < data.length; i += this.CODE.size) {
+
+      const dataChunk = data.slice(i, i + this.CODE.size);
+      
+      for (let key in this.CODE) {
+        if (this.CODE.hasOwnProperty(key) && this.CODE[key].toString() === dataChunk)
+          // console.log(`current ${dataChunk}->${key}`);
+          decoded += key;
+      }
+      // console.log(decoded);
+    }
+
+    this._saveDecoded('DECODED', decoded);
+    console.log(`Saved decoded file to [DECODED].\n`);
+  }
+
+  save(encodefileName, codeFileName) {
+    this._saveCode(codeFileName);
+    console.log(`Saved coded file to [${codeFileName}].`);
+    this._saveEncoded(encodefileName);
+    console.log(`Saved encoded file to [${encodefileName}].\n`);
+  }
+
+  load(encodedFileName, codeFileName) {
+    this.GET_CODE(codeFileName);
+    this.GET_BYTES(encodedFileName);
+  }
+
+  _saveCode(fileName) {
+    let code = '';
+    for (let key in this.CODE) {
+      code += `${key}:${this.CODE[key].toString()}\n`;
+    }
+    fs.writeFileSync(fileName, code);
+  }
+
+  _saveDecoded(fileName, text) {
+    fs.writeFileSync(fileName, text);
+  }
+
+  _saveEncoded(fileName) {
+    fs.writeFileSync(fileName, new Buffer(this.BYTES));
+  }
+
+
+
 }
 
 module.exports = Compressor;
